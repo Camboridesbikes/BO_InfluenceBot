89274d683781851832a7f0504b06027cabdc2075
Cameron H <hattendorfcameron@gmail.com>
2022-02-19 00:22:51 -0800

    Revert "debugging addInfluence"
    This reverts commit 77ef6a5d1ba2704a598b9d9b461a320796d631cb.

--- src/modules/getMemberData.ts
+++ src/modules/getMemberData.ts
@@ -2,7 +2,7 @@ import MemberModel from "../database/models/MemberModel"
 
 export const getMemberData = async(id : string) => {
     const memberData = 
-    (await MemberModel.findOne({discordId: id})) ||
+    (await MemberModel.findOne({discordId: {id}})) ||
     (await MemberModel.create({
         discordId: id,
         totalInfluence: 0,
