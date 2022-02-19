41847724b8ad150fe4575dbc92df06973e590e61
Cameron H <hattendorfcameron@gmail.com>
2022-02-19 14:32:05 -0800

    testing agenda schedule

--- src/bot.ts
+++ src/bot.ts
@@ -33,7 +33,7 @@ import { onInteraction } from "./events/onInteraction";
         
         agenda.on('ready', async () => {
             await agenda.start();
-            await agenda.every('*/1 * * * * *','test ping');
+            await agenda.every('1 minute','test ping');
             await agenda.every("* 10 */1 * *","post daily leaderboard", {timezone : "Europe/London"} );
             await agenda.every("* 10 * 1 *","post weekly leaderboard", {timezone : "Europe/London"} );
         })
