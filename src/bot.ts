43a7daec4dbc1fe0945ec58365f6376fcfda8a74
Cameron H <hattendorfcameron@gmail.com>
2022-02-21 01:38:34 -0800

    testing scheduler

--- src/bot.ts
+++ src/bot.ts
@@ -33,8 +33,8 @@ import { onInteraction } from "./events/onInteraction";
         
         agenda.on('ready', async () => {
             await agenda.start();
-            await agenda.every("37 9 */1 * *","post daily leaderboard", {timezone : "Europe/London"} );
-            await agenda.every("* 10 * 1 *","post weekly leaderboard", {timezone : "Europe/London"} );
+            await agenda.every("40 9 * * *","post daily leaderboard", {timezone : "Europe/London"} );
+            await agenda.every("5 10 * 1 *","post weekly leaderboard", {timezone : "Europe/London"} );
         })
         
     })
